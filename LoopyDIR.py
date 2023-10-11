468c8add7e047177e167a2efca4ee19f9b933b2b
Loopy <114224494+Looopyyy@users.noreply.github.com>
2023-10-11 19:37:56 +0200

    Add files via upload

---
+++ LoopyDIR.py
@@ -0,0 +1,37 @@
+import requests
+print("""
+ ██▓     ▒█████   ▒█████   ██▓███ ▓██   ██▓   ▓█████▄  ██▓ ██▀███  
+▓██▒    ▒██▒  ██▒▒██▒  ██▒▓██░  ██▒▒██  ██▒   ▒██▀ ██▌▓██▒▓██ ▒ ██▒
+▒██░    ▒██░  ██▒▒██░  ██▒▓██░ ██▓▒ ▒██ ██░   ░██   █▌▒██▒▓██ ░▄█ ▒
+▒██░    ▒██   ██░▒██   ██░▒██▄█▓▒ ▒ ░ ▐██▓░   ░▓█▄   ▌░██░▒██▀▀█▄  
+░██████▒░ ████▓▒░░ ████▓▒░▒██▒ ░  ░ ░ ██▒▓░   ░▒████▓ ░██░░██▓ ▒██▒
+░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒▓▒░ ░  ░  ██▒▒▒     ▒▒▓  ▒ ░▓  ░ ▒▓ ░▒▓░
+░ ░ ▒  ░  ░ ▒ ▒░   ░ ▒ ▒░ ░▒ ░     ▓██ ░▒░     ░ ▒  ▒  ▒ ░  ░▒ ░ ▒░
+  ░ ░   ░ ░ ░ ▒  ░ ░ ░ ▒  ░░       ▒ ▒ ░░      ░ ░  ░  ▒ ░  ░░   ░ 
+    ░  ░    ░ ░      ░ ░           ░ ░           ░     ░     ░     
+                                   ░ ░         ░                                                                                                     
+""")
+user_input = input("Url : ")
+user_input = user_input + "/"
+r = requests.get('{}'.format(user_input))
+print('[{}] {} successfully witch {}{}'.format(0,user_input, r.status_code, " ✔️"))
+
+diccionario = open('./LoopyDIR/directorysmall.txt', 'r')
+diccionario_lines = diccionario.readlines()
+
+count = 0
+for direc in diccionario_lines:
+        count += 1
+        direc = ''.join(direc.splitlines())
+        r_direct_format = user_input + direc
+        s_direct_format = user_input + direc + "/"
+        r = requests.get('{}'.format(r_direct_format))
+        s = requests.get('{}'.format(s_direct_format))
+        if r.status_code == 200:
+            print('[{}] {} successfully witch {}{}'.format(count, r_direct_format, r.status_code, " ✔️"))
+        else:
+            print('[{}] {} error witch {}{}'.format(count, r_direct_format, r.status_code, " ❌"))
+        if s.status_code == 200:
+            print('[{}] {} successfully witch {}{}'.format(count, s_direct_format, r.status_code, " ✔️"))
+        else:
+            print('[{}] {} error witch {}{}'.format(count, r_direct_format, r.status_code, " ❌"))
